e67ab1150f4bd681a54012e31d6a05183fdfb68d
Julsan26 <Jmagaju@gmail.com>
2019-10-03 16:24:54 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Logo from "./yeti.jpg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <img src={Logo} className="App-logo" alt="logo" />
+
+        <h1>SignUp</h1>
+            <p>
+              First name:
+              <input type="text" />
+            </p>
+            <p>
+              Last name:
+              <input type="text" />
+           </p>
+            <p>
+              E-mail:
+              <input type="text" />
+            </p>
+            <p>
+              DOB:
+              <input type="text" />
+            </p>
+         
+            <p>
+              Phone Number
+              <input id="phoneNumber" type="tel"></input>
+            </p>
+         
+
+        <div className="box-field">
+          <p>
+            GC
+            <input type="file" className="gc" alt="green" />
+          </p>
+          <p>
+            State ID
+            <input type="file" />
+          </p>
+          <p>
+            SSN
+            <input type="file" />
+          </p>
+          <input
+            type="button"
+            onclick="myFunction()"
+            value="Submit form"
+          ></input>
+        </div>
+      </form>
+    );
+  }
 }
 
-export default App;
+export default MyForm;
